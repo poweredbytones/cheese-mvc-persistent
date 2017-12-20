2d8401ce7b9af1fb1b594146839d39ac116e1a9f
Anthony Fortner <poweredbytones@gmail.com>
2017-12-19 22:58:12 -0600

    completed all needed steps for part-one

--- src/main/java/org/launchcode/models/data/CategoryDao.java
+++ src/main/java/org/launchcode/models/data/CategoryDao.java
@@ -1,5 +1,6 @@
 package org.launchcode.models.data;
 
+import org.launchcode.models.Category;
 import org.launchcode.models.Cheese;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -11,6 +12,6 @@ import javax.transaction.Transactional;
  */
 @Repository
 @Transactional
-public interface CheeseDao extends CrudRepository<Cheese, Integer> {
+public interface CategoryDao extends CrudRepository<Category, Integer> {
 
 }
